49eb51ef0cf4ef0953d362bcb4d6f18626781651
Awais Akram Mughal <iamawaisakram@gmail.com>
2018-02-09 02:25:12 +0500

    debugging and adding foreign key

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -15,8 +15,9 @@ class ArticlesController < ApplicationController
 		
 	end
 	def create
-		
+
 		@article = Article.new(article_params)
+		@article.user = User.first
 		if @article.save
 			flash[:success] = "Article was Successfully created"
 			redirect_to article_path(@article)
@@ -26,7 +27,6 @@ class ArticlesController < ApplicationController
 	end
 
 	def update
-		
 		if @article.update(article_params)
 			flash[:success] = "Article was successfully updated"
 			redirect_to article_path(@article)
