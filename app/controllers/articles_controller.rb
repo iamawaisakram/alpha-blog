9b64df5ed8fb972d40f9b3f70f05b95b048a95f7
Awais Akram Mughal <iamawaisakram@gmail.com>
2018-02-11 02:17:10 +0500

    Removed Vunerabilities for users and articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,9 +1,13 @@
 class ArticlesController < ApplicationController
 	before_action :set_article, only: [:edit, :update, :show, :destroy]
+	before_action :require_user, except: [:index, :show]
+	before_action :require_same_user, only: [:edit, :update, :destroy]
+	
+
+
 
 	def index
-	@articles = Article.paginate(page: params[:page], per_page: 3)
-
+	@articles = Article.paginate(page: params[:page], per_page: 5)
 	end
 
 
@@ -14,6 +18,7 @@ class ArticlesController < ApplicationController
 	def edit
 		
 	end
+
 	def create
 
 		@article = Article.new(article_params)
@@ -34,13 +39,11 @@ class ArticlesController < ApplicationController
 			render 'edit'
 		end
 	end
+	
 	def show
-		
-
 	end
 
-	def destroy
-		
+	def destroy	
 		@article.destroy
 		flash[:danger] = "Article was successfully deleted"
 		redirect_to articles_path
@@ -50,7 +53,15 @@ class ArticlesController < ApplicationController
 		def set_article
 			@article = Article.find(params[:id])
 		end
+		
 		def article_params
 			params.require(:article).permit(:title, :description)
+		end
+
+		def require_same_user
+			if current_user != @article.user
+				flash[:danger] = "You can only edit or delte your own articls."
+				redirect_to root_path
+			end
 		end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -12,4 +12,8 @@ Rails.application.routes.draw do
 
   # 2nd Method
   resources :users, except: [:new]
+
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
 end
